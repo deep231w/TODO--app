326b5ed0e99f48c4eae15b6a6fa0c1d26d8741d9
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2024-06-11 00:49:23 +0530

    db logic

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,10 @@
 const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://maharanad449:jDeK4p9xB5TN4r7r@todo--app.qkkwzvl.mongodb.net/todos");
+const todoschema=mongoose.Schema({
+    title:String,
+    description:String,
+    completed:Boolean
+})
+const todo=mongoose.model('todo',todoschema);
+
+module.exports=todo;

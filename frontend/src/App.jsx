4468dd00d372e20514b16581aff70fc8f1cb2d38
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2024-06-11 14:00:54 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { CreateTodo } from './components/CreateTodo'
+
+function App() {
+
+  return (
+    <div>
+     <CreateTodo></CreateTodo>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,9 @@
+export function CreateTodo(){
+    return
+    <div>
+        <input type="text" placeholder="title"></input> <br />
+        <input type="text" placeholder="title"></input> <br />
+        <button>Add todo</button>
+    </div>
+}
+// module.exports = CreateTodo
